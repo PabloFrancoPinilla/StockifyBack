6d80c3e14427968891cb5a92748133469e8eafd4
PabloFrancoPinilla <francopablo169@gmail.com>
2024-06-04 23:18:03 +0200

    Delete Fixed

--- Data/Context.cs
+++ Data/Context.cs
@@ -1,59 +1,59 @@
 ﻿
-namespace Stockify.Data{
-  using Microsoft.EntityFrameworkCore;
-using Stockify.Models;
-
-
-public class StockifyContext :DbContext
+namespace Stockify.Data
 {
-     public StockifyContext(DbContextOptions<StockifyContext> options)
-            : base(options)
-        {
-        }
-          protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-          
+  using Microsoft.EntityFrameworkCore;
+  using Stockify.Models;
 
-             modelBuilder.Entity<Product>()
-                .HasOne(p => p.Inventory)
-                .WithMany(i => i.Products)
-                .HasForeignKey(p => p.InventoryId)
-                .OnDelete(DeleteBehavior.NoAction);  // No action on delete
 
-            modelBuilder.Entity<Category>()
-                .HasOne(c => c.Inventory)
-                .WithMany(i => i.Categories)
-                .HasForeignKey(c => c.InventoryId)
-                .OnDelete(DeleteBehavior.NoAction);  // No action on delete
+  public class StockifyContext : DbContext
+  {
+    public StockifyContext(DbContextOptions<StockifyContext> options)
+           : base(options)
+    {
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
 
-            modelBuilder.Entity<ProductCategory>()
-                .HasKey(pc => new { pc.ProductId, pc.CategoryId });
 
-            modelBuilder.Entity<ProductCategory>()
-                .HasOne(pc => pc.Product)
-                .WithMany(p => p.ProductCategories)
-                .HasForeignKey(pc => pc.ProductId)
-                .OnDelete(DeleteBehavior.Cascade);
+      modelBuilder.Entity<Product>()
+ .HasOne(p => p.Inventory)
+ .WithMany(i => i.Products)
+ .HasForeignKey(p => p.InventoryId)
+ .OnDelete(DeleteBehavior.NoAction);  // No action on delete
 
-            modelBuilder.Entity<ProductCategory>()
-                .HasOne(pc => pc.Category)
-                .WithMany(c => c.ProductCategories)
-                .HasForeignKey(pc => pc.CategoryId)
-                .OnDelete(DeleteBehavior.Cascade);
+      modelBuilder.Entity<Category>()
+          .HasOne(c => c.Inventory)
+          .WithMany(i => i.Categories)
+          .HasForeignKey(c => c.InventoryId)
+          .OnDelete(DeleteBehavior.NoAction);  // No action on delete
 
-            
+      modelBuilder.Entity<ProductCategory>()
+          .HasKey(pc => new { pc.ProductId, pc.CategoryId });
 
-        }
+      modelBuilder.Entity<ProductCategory>()
+          .HasOne(pc => pc.Product)
+          .WithMany(p => p.ProductCategories)
+          .HasForeignKey(pc => pc.ProductId)
+          .OnDelete(DeleteBehavior.Cascade);
 
-            public DbSet<Inventory> Inventories { get; set; }
-            public DbSet<Category> Categories {get; set;}
-            public DbSet<Product> Products {get;set;}
-            public DbSet<ProductCategory> ProductsCategory {get; set;}
-            public DbSet<Tenant> Tenants {get; set; }
-            public DbSet<User> Users {get; set;}    
-            public DbSet<Transaction> Transactions {get; set; }
-            
-        
+      modelBuilder.Entity<ProductCategory>()
+          .HasOne(pc => pc.Category)
+          .WithMany(c => c.ProductCategories)
+          .HasForeignKey(pc => pc.CategoryId)
+          .OnDelete(DeleteBehavior.Cascade);
 
-}
+
+    }
+
+    public DbSet<Inventory> Inventories { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<ProductCategory> ProductsCategory { get; set; }
+    public DbSet<Tenant> Tenants { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+
+
+
+  }
 }
--- Data/InventoryRepository.cs
+++ Data/InventoryRepository.cs
@@ -53,7 +53,7 @@ public class InventoryRepository : IInventoryRepository
             // Manejar el caso en el que no se puede encontrar el TenantId en el token JWT
             return new List<InventoryDto>();
         }
-         int tenantId = Convert.ToInt32(tenantIdClaim.Value);
+        int tenantId = Convert.ToInt32(tenantIdClaim.Value);
         var inventories = _context.Inventories.Include(p => p.Products).ThenInclude(p => p.ProductCategories)
                 .ThenInclude(pc => pc.Category).Where(p => p.TenantId == tenantId).ToList();
         var inventoriesDto = inventories.Select(p => new InventoryDto
@@ -90,9 +90,31 @@ public class InventoryRepository : IInventoryRepository
     }
     public void Delete(int id)
     {
-        var inventory = _context.Inventories.Find(id);
-        _context.Remove(inventory);
-        SaveChanges();
+        var inventory = _context.Inventories
+       .Include(i => i.Products)
+       .ThenInclude(p => p.ProductCategories)
+       .Include(i => i.Categories)
+       .FirstOrDefault(i => i.Id == id);
+
+        if (inventory != null)
+        {
+            // Eliminar las relaciones de ProductCategory
+            foreach (var product in inventory.Products)
+            {
+                _context.ProductsCategory.RemoveRange(product.ProductCategories);
+            }
+
+            // Eliminar los productos
+            _context.Products.RemoveRange(inventory.Products);
+
+            // Eliminar las categorías
+            _context.Categories.RemoveRange(inventory.Categories);
+
+            // Eliminar el inventario
+            _context.Inventories.Remove(inventory);
+
+            _context.SaveChanges();
+        }
     }
     public void SaveChanges()
     {
